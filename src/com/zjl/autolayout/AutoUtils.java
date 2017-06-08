ea7a59ef56febea2b590e9c680ee03103a15a884
zhaoya <zhaoya@qiyi.com>
2017-06-08 15:27:04 +0800

    bugfix
    Signed-off-by: zhaoya188 <zhaoya188@qq.com>

--- src/com/zjl/autolayout/AutoUtils.java
+++ src/com/zjl/autolayout/AutoUtils.java
@@ -1,117 +1,126 @@
 package com.zjl.autolayout;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Resources;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.TextView;
 
-/**
- * 
- * @author ZhengJingle
- *
- */
+import java.lang.reflect.Field;
 
 public class AutoUtils {
 
-	private static int displayWidth;
-	private static int displayHeight;
-	
-	private static int designWidth;
-	private static int designHeight;
-	
-	private static double textPixelsRate;
-	
-	public static void setSize(Activity act, boolean hasStatusBar, int designWidth, int designHeight){
-		if(act==null || designWidth<1 || designHeight<1)return;
-		
-        Display display = act.getWindowManager().getDefaultDisplay();
-		int width = display.getWidth();
-        int height = display.getHeight();
-        
-		if (hasStatusBar) {
-			height -= getStatusBarHeight(act);
-		}
-		
-		AutoUtils.displayWidth=width;
-		AutoUtils.displayHeight=height;
-		
-		AutoUtils.designWidth=designWidth;
-		AutoUtils.designHeight=designHeight;
-		
-		double displayDiagonal=Math.sqrt(Math.pow(AutoUtils.displayWidth, 2)+Math.pow(AutoUtils.displayHeight, 2));
-		double designDiagonal=Math.sqrt(Math.pow(AutoUtils.designWidth, 2)+Math.pow(AutoUtils.designHeight, 2));
-		AutoUtils.textPixelsRate=displayDiagonal/designDiagonal;	
-	}
-	
-    public static int getStatusBarHeight(Context context)
-    {
-		int result = 0;
-		try {
-			int resourceId = context.getResources().getIdentifier(
-					"status_bar_height", "dimen", "android");
-			if (resourceId > 0) {
-				result = context.getResources().getDimensionPixelSize(
-						resourceId);
-			}
-		} catch (Resources.NotFoundException e) {
-			e.printStackTrace();
-		}
-		return result;
-    }
-    
-    public static void auto(Activity act){
-    	if(act==null || displayWidth<1 || displayHeight<1)return;
-    	
-    	View view=act.getWindow().getDecorView();
-    	auto(view);
-    }
-    
-    public static void auto(View view){
-    	if(view==null || displayWidth<1 || displayHeight<1)return;
-    	
-    	AutoUtils.autoTextSize(view);
-    	AutoUtils.autoSize(view);
-    	AutoUtils.autoPadding(view);
-    	AutoUtils.autoMargin(view);
-    	
-    	if(view instanceof ViewGroup){
-    		auto((ViewGroup)view);
-    	}
-    	
-    }
-    
-    private static void auto(ViewGroup viewGroup){
-    	int count = viewGroup.getChildCount();
-    	
-		for (int i = 0; i < count; i++) {
+    private static int displayWidth;
+    private static int displayHeight;
 
-			View child = viewGroup.getChildAt(i);
-			
-			if(child!=null){
-				auto(child);
-			}
-		}
+    private static int designWidth;
+    private static int designHeight;
+
+    private static double textPixelsRate;
+
+    public static void setSize(Activity act, boolean hasStatusBar, int designWidth, int designHeight) {
+        if (act == null || designWidth < 1 || designHeight < 1) return;
+
+        Display display = act.getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
+        int height = display.getHeight();
+
+        if (hasStatusBar) {
+            height -= getStatusBarHeight(act);
+        }
+
+        AutoUtils.displayWidth = width;
+        AutoUtils.displayHeight = height;
+
+        AutoUtils.designWidth = designWidth;
+        AutoUtils.designHeight = designHeight;
+
+        double displayDiagonal = Math.sqrt(Math.pow(AutoUtils.displayWidth, 2) + Math.pow(AutoUtils.displayHeight, 2));
+        double designDiagonal = Math.sqrt(Math.pow(AutoUtils.designWidth, 2) + Math.pow(AutoUtils.designHeight, 2));
+        AutoUtils.textPixelsRate = displayDiagonal / designDiagonal;
     }
-    
-    public static void autoMargin(View view){
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        try {
+            int resourceId = context.getResources().getIdentifier(
+                    "status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = context.getResources().getDimensionPixelSize(
+                        resourceId);
+            }
+        } catch (Resources.NotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void auto(Window window) {
+        if (window == null || displayWidth < 1 || displayHeight < 1) return;
+
+        View view = window.getDecorView();
+        auto(view);
+    }
+
+    public static void auto(Dialog dialog) {
+        if (dialog == null || displayWidth < 1 || displayHeight < 1) return;
+
+        View view = dialog.getWindow().getDecorView();
+        auto(view);
+    }
+
+    public static void auto(Activity act) {
+        if (act == null || displayWidth < 1 || displayHeight < 1) return;
+
+        View view = act.getWindow().getDecorView();
+        auto(view);
+    }
+
+    public static void auto(View view) {
+        if (view == null || displayWidth < 1 || displayHeight < 1) return;
+
+        AutoUtils.autoTextSize(view);
+        AutoUtils.autoSize(view);
+        AutoUtils.autoPadding(view);
+        AutoUtils.autoMargin(view);
+
+        if (view instanceof ViewGroup) {
+            auto((ViewGroup) view);
+        }
+    }
+
+    private static void auto(ViewGroup viewGroup) {
+        int count = viewGroup.getChildCount();
+
+        for (int i = 0; i < count; i++) {
+
+            View child = viewGroup.getChildAt(i);
+
+            if (child != null) {
+                auto(child);
+            }
+        }
+    }
+
+    public static void autoMargin(View view) {
         if (!(view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams))
             return;
 
         ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
-        if(lp == null)return ;
+        if (lp == null) return;
 
         lp.leftMargin = getDisplayWidthValue(lp.leftMargin);
         lp.topMargin = getDisplayHeightValue(lp.topMargin);
         lp.rightMargin = getDisplayWidthValue(lp.rightMargin);
         lp.bottomMargin = getDisplayHeightValue(lp.bottomMargin);
-        
     }
 
-    public static void autoPadding(View view){
+    public static void autoPadding(View view) {
         int l = view.getPaddingLeft();
         int t = view.getPaddingTop();
         int r = view.getPaddingRight();
@@ -125,41 +134,74 @@ public class AutoUtils {
         view.setPadding(l, t, r, b);
     }
 
-    public static void autoSize(View view){
+    public static Object getField(Object obj, Class cls, String fields) {
+        try {
+            Field field = cls.getDeclaredField(fields);
+            field.setAccessible(true);
+            return field.get(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean setField(Object obj, Class cls, String fields, Object value) {
+        try {
+            Field field = cls.getDeclaredField(fields);
+            field.setAccessible(true);
+            field.set(obj, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void autoSize(View view) {
         ViewGroup.LayoutParams lp = view.getLayoutParams();
 
         if (lp == null) return;
-        
-        if(lp.width>0){
-        	lp.width = getDisplayWidthValue(lp.width);
+
+        if (lp.width > 0) {
+            if (lp.width >= 2)
+                lp.width = getDisplayWidthValue(lp.width);
         }
 
-        if(lp.height>0){
-        	lp.height = getDisplayHeightValue(lp.height);
+        if (lp.height > 0) {
+            if (lp.height >= 2)
+                lp.height = getDisplayHeightValue(lp.height);
         }
-        
-    }
-    
-    public static void autoTextSize(View view){
-    	if(view instanceof TextView){
-    		double designPixels=((TextView) view).getTextSize();
-    		double displayPixels=textPixelsRate*designPixels;
-    		((TextView) view).setIncludeFontPadding(false);
-    		((TextView) view).setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) displayPixels);
-    	}
+
+        Integer width = (Integer) getField(view, View.class, "mMinWidth");
+        if (width != null && width > 0) {
+            view.setMinimumWidth(getDisplayWidthValue(width));
+        }
+        Integer height = (Integer) getField(view, View.class, "mMinHeight");
+        if (height != null && height > 0) {
+            view.setMinimumHeight(getDisplayHeightValue(height));
+        }
     }
 
-    public static int getDisplayWidthValue(int designWidthValue){
-    	if(designWidthValue<2){
-    		return designWidthValue;
-    	}
+    public static void autoTextSize(View view) {
+        if (view instanceof TextView) {
+            double designPixels = ((TextView) view).getTextSize();
+            double displayPixels = designPixels * displayHeight / designHeight;//textPixelsRate * designPixels;
+            //((TextView) view).setIncludeFontPadding(false);
+            ((TextView) view).setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) displayPixels);
+
+            Float mSpacingMult = (Float) getField(view, TextView.class, "mSpacingMult");
+            Float mSpacingAdd = (Float) getField(view, TextView.class, "mSpacingAdd");
+            if (mSpacingMult != null && mSpacingAdd != null) {
+                ((TextView) view).setLineSpacing(mSpacingAdd * displayHeight / designHeight, mSpacingMult);
+            }
+        }
+    }
+
+    public static int getDisplayWidthValue(int designWidthValue) {
         return designWidthValue * displayWidth / designWidth;
     }
 
-    public static int getDisplayHeightValue(int designHeightValue){
-    	if(designHeightValue<2){
-    		return designHeightValue;
-    	}
+    public static int getDisplayHeightValue(int designHeightValue) {
         return designHeightValue * displayHeight / designHeight;
     }
 }
